c45b6bea900e680cefa535a6cb4725ec10411cb4
Aniket Waliyan <aniket.wali007@gmail.com>
2021-07-31 20:26:51 +0530

    Calculator Functionality Done.

---
+++ script.js
@@ -0,0 +1,108 @@
+const calculator = {
+	displayValue: '0',
+	firstOperand: null,
+	awaitSecondOperand: false,
+	operator: null
+};
+
+function inputDigit(digit) {
+	let { displayValue, awaitSecondOperand } = calculator;
+
+	if (awaitSecondOperand === true) {
+		calculator.displayValue = digit
+		calculator.awaitSecondOperand = false;
+	} else {
+		calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+	}
+}
+
+function deleteKey() {
+	let { displayValue } = calculator;
+	let buffer = displayValue;
+
+	if (buffer.length === 1) {
+		buffer = '0';
+	} else {
+		buffer = displayValue.substring(0, displayValue.length - 1);
+	}
+	calculator.displayValue = buffer;
+	updateDisplay();
+	console.log(calculator);
+	console.log(displayValue);
+}
+
+function handleOperator(nextOperator) {
+	let { firstOperand, displayValue, operator } = calculator;
+	const inputValue = parseInt(displayValue);
+
+	if (operator && calculator.waitingForSecondOperand) {
+		calculator.operator = nextOperator;
+		return;
+	}
+
+	if (firstOperand === null) {
+		calculator.firstOperand = inputValue;
+	} else if (operator) {
+		const currentValue = firstOperand || 0;
+		const result = getCalculation[operator](currentValue, inputValue);
+
+		calculator.displayValue = String(result);
+		calculator.firstOperand = result;
+	}
+
+	calculator.awaitSecondOperand = true;
+	calculator.operator = nextOperator;
+}
+
+const getCalculation = {
+	'/': (firstOperand, secondOperand) => firstOperand / secondOperand,
+
+	'*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+
+	'+': (firstOperand, secondOperand) => firstOperand + secondOperand,
+
+	'-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+
+	'=': (firstOperand, secondOperand) => secondOperand
+};
+
+function clearState() {
+	calculator.displayValue = '0';
+	calculator.firstOperand = null;
+	calculator.awaitSecondOperand = false;
+	calculator.operator = null;
+	console.log(calculator);
+}
+
+function updateDisplay() {
+	const display = document.querySelector('.screen');
+	display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+const keys = document.querySelector('.keys');
+keys.addEventListener('click', event => {
+	const { target } = event;
+	if (!target.matches('button')) {
+		return;
+	}
+	if (target.classList.contains('operator')) {
+		handleOperator(target.value);
+		updateDisplay();
+		return;
+	}
+	if (target.classList.contains('all-clear')) {
+		clearState();
+		updateDisplay();
+		return;
+	}
+	if (target.classList.contains('backspace')) {
+		deleteKey();
+		updateDisplay();
+		return;
+	}
+
+	inputDigit(target.value);
+	updateDisplay();
+});
